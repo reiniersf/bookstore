0881f597a6222f1c57778028b01043a05f81185c
R.S.F <reytosan0088@gmail.com>
2017-09-05 09:01:30 +0000

    Switch to Hibernate JPA implementation

--- bookstore-domain/src/main/java/cu/pdi/bookstore/config/AppPersistenceConfig.java
+++ bookstore-domain/src/main/java/cu/pdi/bookstore/config/AppPersistenceConfig.java
@@ -2,23 +2,31 @@ package cu.pdi.bookstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.instrument.classloading.LoadTimeWeaver;
-import org.springframework.instrument.classloading.SimpleLoadTimeWeaver;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by taiyou
  * on 9/3/17.
  */
 @Configuration
+@EnableTransactionManagement
 public class AppPersistenceConfig {
 
+
+    @Bean public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
@@ -28,17 +36,22 @@ public class AppPersistenceConfig {
         emfb.setDataSource(dataSource);
         emfb.setJpaVendorAdapter(jpaVendorAdapter);
         emfb.setPackagesToScan("cu.pdi.bookstore.domain");
-        emfb.setLoadTimeWeaver(new SimpleLoadTimeWeaver());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.hbm2ddl.auto", "create");
+
+        emfb.setJpaProperties(jpaProperties);
+
         return emfb;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
-        EclipseLinkJpaVendorAdapter adapter = new EclipseLinkJpaVendorAdapter();
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
         adapter.setDatabase(Database.POSTGRESQL);
         adapter.setShowSql(true);
         adapter.setGenerateDdl(false);
-        adapter.setDatabasePlatform("org.eclipse.persistence.platform.database.PostgreSQLPlatform");
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
         return adapter;
     }
 
